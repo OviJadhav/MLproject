e0371f56966241b4dcbd2e652f5d128a1e953e68
insert github real name here <insert github email here>
2023-11-23 05:04:36 -0800

    EDA and Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging 
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -15,5 +16,3 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
-    
-
